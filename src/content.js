8a84de35bc6451af038e66461e105c7cdcd6b7c0
Emiliano <emiliano.cabello7@iems.edu.mx>
2024-07-26 09:51:13 -0600

    feat: :art: add new image containers for the images on the home page
    it's for to improve the layout
    
    I've added a new method in the content object for to return the new element itself

--- src/content.js
+++ src/content.js
@@ -8,16 +8,21 @@ import imgReserve from './assets/reservation.webp';
 import imgAbout from './assets/about.webp';
 
 function CreateChildElements(parent, child) {
+
     class Content {
+        savedId;
         constructor(parent, child) {
             this.parent = parent;
             this.child = child;
         }
-        AddId(id) {
-            const childId = document.createElement(child);
+        AddId(id){
+            const childId = document.createElement(this.child);
             childId.setAttribute('id', id);
             this.parent.appendChild(childId);
-            return { childId: id };
+            this.savedId = {
+                getId: id,
+                get getElement () { return document.getElementById(this.getId)}
+            };
 
         }
 
@@ -54,30 +59,39 @@ function HomeModule() {
     reserveText.AddId('reserve-text');
     reserveText.AddTextContent('reserve-text', 'RESERVATION');
 
-    const ourRestaurant =
-        CreateChildElements(divHeader, 'p');
+    const ourRestaurant = CreateChildElements(divHeader, 'p');
     ourRestaurant.AddId('our-restaurant');
     ourRestaurant.AddTextContent('our-restaurant', 'OUR RESTAURANT');
 
-    const video = CreateChildElements(divHeader, 'video');
-    const videoId = video.AddId('header-video');
+    //containers
+    const videoContainer = CreateChildElements(divHeader, 'div');
+    videoContainer.AddId('video-container');
+    const video = CreateChildElements(videoContainer.savedId.getElement, 'video');
+    video.AddId('header-video');
     video.AddAttribute('header-video', 'autoplay');
     video.AddAttribute('header-video', 'loop')
 
-    const videoSrc = CreateChildElements(document.getElementById(videoId.childId), 'source');
+    const videoSrc = CreateChildElements(video.savedId.getElement, 'source');
     videoSrc.AddId('show-video');
     videoSrc.AddAttribute('show-video', 'src', './Video.mp4');
     videoSrc.AddAttribute('show-video', 'type', 'video/mp4');
 
-    const menu = CreateChildElements(divHeader, 'img');
+    const menuContainer = CreateChildElements(divHeader, 'div');
+    menuContainer.AddId('container-menu');
+    const menu = CreateChildElements(menuContainer.savedId.getElement, 'img');
     menu.AddId('img-menu');
     menu.AddAttribute('img-menu', 'src', imgMenu);
 
-    const reservation = CreateChildElements(divHeader, 'img');
+    const reserveContainer = CreateChildElements(divHeader, 'div');
+    reserveContainer.AddId('container-reserve');
+    const reservation = CreateChildElements(reserveContainer.savedId.getElement, 'img');
     reservation.AddId('img-reserve');
     reservation.AddAttribute('img-reserve', 'src', imgReserve);
 
-    const aboutUs = CreateChildElements(divHeader, 'img');
+    const aboutContainer = CreateChildElements(divHeader, 'div');
+    aboutContainer.AddId('container-about');
+    const aboutUs = CreateChildElements(aboutContainer.savedId.getElement, 'img');
     aboutUs.AddId('img-about');
     aboutUs.AddAttribute('img-about', 'src', imgAbout);
+
 };
