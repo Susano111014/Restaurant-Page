336a17da4239e0c943be600aae2a9bfc1d917fc1
Emiliano <emiliano.cabello7@iems.edu.mx>
2024-07-26 18:59:12 -0600

    feat: :art: Append the all text elements for within its associative container-section in the home-page
    I've sort the containers  variables for problems of its childs elements variable's declaration.

--- src/content.js
+++ src/content.js
@@ -45,27 +45,32 @@ function CreateChildElements(parent, child) {
 }
 
 function HomeModule() {
+    //containers
     const divHeader = document.getElementById('content');
+    const videoContainer = CreateChildElements(divHeader, 'div');
+    videoContainer.AddId('video-container');
 
-    const headerTitle = CreateChildElements(divHeader, 'h1');
-    headerTitle.AddId('Title');
-    headerTitle.AddTextContent('Title', 'Restaurant');
+    const reserveContainer = CreateChildElements(divHeader, 'div');
+    reserveContainer.AddId('container-reserve');
 
-    const headerText = CreateChildElements(divHeader, 'p');
+    const menuContainer = CreateChildElements(divHeader, 'div');
+    menuContainer.AddId('container-menu');
+
+    const aboutContainer = CreateChildElements(divHeader, 'div');
+    aboutContainer.AddId('container-about');
+
+    const headerText = CreateChildElements(menuContainer.savedId.getElement, 'p');
     headerText.AddId('menu-text');
     headerText.AddTextContent('menu-text', 'MENU');
 
-    const reserveText = CreateChildElements(divHeader, 'p');
+    const reserveText = CreateChildElements(reserveContainer.savedId.getElement, 'p');
     reserveText.AddId('reserve-text');
     reserveText.AddTextContent('reserve-text', 'RESERVATION');
 
-    const ourRestaurant = CreateChildElements(divHeader, 'p');
+    const ourRestaurant = CreateChildElements(aboutContainer.savedId.getElement, 'p');
     ourRestaurant.AddId('our-restaurant');
     ourRestaurant.AddTextContent('our-restaurant', 'OUR RESTAURANT');
 
-    //containers
-    const videoContainer = CreateChildElements(divHeader, 'div');
-    videoContainer.AddId('video-container');
     const video = CreateChildElements(videoContainer.savedId.getElement, 'video');
     video.AddId('header-video');
     video.AddAttribute('header-video', 'autoplay');
@@ -76,20 +81,14 @@ function HomeModule() {
     videoSrc.AddAttribute('show-video', 'src', './Video.mp4');
     videoSrc.AddAttribute('show-video', 'type', 'video/mp4');
 
-    const menuContainer = CreateChildElements(divHeader, 'div');
-    menuContainer.AddId('container-menu');
     const menu = CreateChildElements(menuContainer.savedId.getElement, 'img');
     menu.AddId('img-menu');
     menu.AddAttribute('img-menu', 'src', imgMenu);
 
-    const reserveContainer = CreateChildElements(divHeader, 'div');
-    reserveContainer.AddId('container-reserve');
     const reservation = CreateChildElements(reserveContainer.savedId.getElement, 'img');
     reservation.AddId('img-reserve');
     reservation.AddAttribute('img-reserve', 'src', imgReserve);
 
-    const aboutContainer = CreateChildElements(divHeader, 'div');
-    aboutContainer.AddId('container-about');
     const aboutUs = CreateChildElements(aboutContainer.savedId.getElement, 'img');
     aboutUs.AddId('img-about');
     aboutUs.AddAttribute('img-about', 'src', imgAbout);
@@ -97,8 +96,10 @@ function HomeModule() {
     const homePageImg = Array.from([menu.savedId.getElement,
     reservation.savedId.getElement,
     aboutUs.savedId.getElement]);
-
     associateClass(homePageImg, 'HomePage-img');
+
+    const navElement = document.querySelector('.Header-Nav');
+    video.savedId.getElement.appendChild(navElement);
 };
 
 function associateClass(myArray, Class) {
